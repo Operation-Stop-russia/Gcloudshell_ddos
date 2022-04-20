3f5b37507fe4bf6b14f0c5713a01a32c612dbf95
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-20 05:57:12 +0300

    Update Start_L4.sh

--- Start_L4.sh
+++ Start_L4.sh
@@ -4,7 +4,7 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
-threads="${1:-700}"; threads="-t $threads"
+threads="${1:-600}"; threads="-t $threads"
 rpc="--rpc 1000"
 debug="--debug"
 
