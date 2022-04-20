62863aae5d3d3909e62ac167b279918673fd37a7
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-20 12:12:10 +0300

    Update Start_L7m.sh

--- Start_L7m.sh
+++ Start_L7m.sh
@@ -11,7 +11,8 @@ debug="--debug"
 
 while true
 do
-   pkill -f runner.py 
+   pkill -f runner.py
+   sleep 21
    list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]" | wc -l)
    while [[ $list_size = "0"  ]]
       do
