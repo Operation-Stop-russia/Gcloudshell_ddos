93572d1671de189cbf1503cdf5cb8aff96300b63
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-21 10:55:56 +0300

    Delete Start_db1000n.sh

--- Start_db1000n.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#curl -L https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_db1000n.sh | bash
-
-curl https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/install_db1000n_GShell.sh
-sudo ./db1000n
