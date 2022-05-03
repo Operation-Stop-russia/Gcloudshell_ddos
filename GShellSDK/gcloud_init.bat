08e5fb175b7e347e5cd043275daf0c944a1d683f
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-04 02:26:15 +0300

    Update gcloud_init.bat

--- GShellSDK/gcloud_init.bat
+++ GShellSDK/gcloud_init.bat
@@ -1,5 +1,5 @@
 @Echo off
-FOR /L %%G IN (1,1,100) DO (
-(echo 1 && echo %%G && echo n) | gcloud init
+FOR /L %%G IN (2,1,100) DO (
+(echo 1 && echo %%G && echo n) | gcloud init --skip-diagnostics
 )
 Exit
