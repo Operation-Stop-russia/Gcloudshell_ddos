501bc061545edcaca03cdeb87f7362f6a65ae288
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-06 16:03:25 +0300

    Update start_sessions.bat

--- GShellSDK/start_sessions.bat
+++ GShellSDK/start_sessions.bat
@@ -1,5 +1,5 @@
 @Echo off
-FOR /L %%G IN (1,1,100) DO (
+FOR /L %%G IN (1,1,50) DO (
 (echo 1 && echo %%G && echo n) | gcloud init --skip-diagnostics
 start cmd.exe /c gcloud cloud-shell ssh --authorize-session
 Pause
