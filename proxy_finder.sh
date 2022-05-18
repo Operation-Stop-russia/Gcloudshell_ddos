dc5e269043f97185a74383f5ee223d32169a0ddd
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-18 20:59:15 +0300

    Create proxy_finder.sh

---
+++ proxy_finder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#curl -l https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/proxy_finder.sh | bash
+rm -rf proxy_finder
+git clone https://github.com/porthole-ascend-cinnamon/proxy_finder
+cd ~/proxy_finder
+python3 -m pip install -r requirements.txt
+
+python3 finder.py --threads 2000
