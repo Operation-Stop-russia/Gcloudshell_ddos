2dae86f836d5b139d9c7518572b1490236c2a118
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-06-09 00:05:47 +0300

    Delete proxy_finder.sh

--- proxy_finder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#curl -L https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/proxy_finder.sh | bash
-rm -rf proxy_finder
-git clone https://github.com/porthole-ascend-cinnamon/proxy_finder
-cd ~/proxy_finder
-python3 -m pip install -r requirements.txt
-
-python3 finder.py --threads 1000
