eb37a3387b6a573d77e493fb99a08440a8de568a
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-07-03 16:31:03 +0300

    Update Start_L7_test.sh

--- Start_L7_test.sh
+++ Start_L7_test.sh
@@ -5,7 +5,7 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
-threads="-t 3600"
+threads="-t 3000"
 rpc="--rpc 500"
 debug=""
 vpn="--vpn 1"
